6389b6ce2949b6fe9872eb40bc525d069bdbd6f1
Mike Diogni <mikediogni@gmail.com>
2025-05-09 21:48:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; // Assurez-vous d'ajouter votre CSS
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='all'>
+      <div className='App'>
+        <header className='App-header'>
+          <div className='navigation-bar'>
+            <div className='logo'>GestionProject</div>
+               
+            
+              <nav>
+                <ul>
+                  <li> <a href='#'>Home</a></li>
+                  <li> <a href='#'>Domain</a></li>
+                  <li> <a href='#'>Project</a></li>
+                  <li> <a href='#'>Contact</a></li>
+                  <li> <a href='#'>login</a></li>
+                </ul>
+              </nav>
+            
+           </div>  
+        </header>
+        <main className="body">
+
+        <div className="search-container">
+        <form onSubmit="">
+          <input
+            type="text"
+            placeholder="Rechercher projets, tâches..."
+            value=""
+            onChange=""
+            className="search-input"
+          />
+          <button type="submit" className="search-button">
+            <i className="search-icon">🔍</i>
+          </button>
+        </form>
+      </div>
+       
+          <h1>Welcome to GestionProject</h1>
+          <p>
+            GestionProject facilitates academic research by providing access to previous academic project and enhancing collaboration among research       </p>
+          <div className="button-container">
+            <button onClick={() => alert('Accéder aux Projets')}>Mes Projets</button>
+            <button onClick={() => alert('Accéder aux Tâches')}>Mes Tâches</button>
+            <button onClick={() => alert('Accéder aux Équipes')}>Mes Équipes</button>
+          </div>
+        </main>
+      </div>
+    </div>  
   );
 }
 
 export default App;
+
