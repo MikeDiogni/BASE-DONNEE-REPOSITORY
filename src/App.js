1ae325c17358911bc719aefafb619b695d549f9a
Mike Diogni <mikediogni@gmail.com>
2025-05-22 11:18:35 +0100

    initialisation

--- src/App.js
+++ src/App.js
@@ -1,57 +1,21 @@
+// src/App.js
 import React from 'react';
-import './App.css'; // Assurez-vous d'ajouter votre CSS
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Accueil from './Pages/Accueil/Accueil';
+import Register from './Pages/Authentification/Register';
+import Login from './Pages/Authentification/Login';
+
 
 function App() {
   return (
-    <div className='all'>
-      <div className='App'>
-        <header className='App-header'>
-          <div className='navigation-bar'>
-            <div className='logo'>GestionProject</div>
-               
-            
-              <nav>
-                <ul>
-                  <li> <a href='#'>Home</a></li>
-                  <li> <a href='#'>Domain</a></li>
-                  <li> <a href='#'>Project</a></li>
-                  <li> <a href='#'>Contact</a></li>
-                  <li> <a href='#'>login</a></li>
-                </ul>
-              </nav>
-            
-           </div>  
-        </header>
-        <main className="body">
-
-        <div className="search-container">
-        <form onSubmit="">
-          <input
-            type="text"
-            placeholder="Rechercher projets, tâches..."
-            value=""
-            onChange=""
-            className="search-input"
-          />
-          <button type="submit" className="search-button">
-            <i className="search-icon">🔍</i>
-          </button>
-        </form>
-      </div>
-       
-          <h1>Welcome to GestionProject</h1>
-          <p>
-            GestionProject facilitates academic research by providing access to previous academic project and enhancing collaboration among research       </p>
-          <div className="button-container">
-            <button onClick={() => alert('Accéder aux Projets')}>Mes Projets</button>
-            <button onClick={() => alert('Accéder aux Tâches')}>Mes Tâches</button>
-            <button onClick={() => alert('Accéder aux Équipes')}>Mes Équipes</button>
-          </div>
-        </main>
-      </div>
-    </div>  
+    <Router>
+      <Routes>
+        <Route path='/' element={<Accueil/>} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
